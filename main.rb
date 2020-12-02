e4a2a4acb620c6dd6ecddbc4ab671ef2dfbef1f7
Miguel Ricaño <mricanho@gmail.com>
2020-12-02 16:00:35 -0600

    Add my_all?

--- main.rb
+++ main.rb
@@ -19,5 +19,14 @@ module Enumerable
     my_each { |x| result << x if yield(x) }
     result
   end
+  def my_all?(*)
+    unless block_given?
+      my_each { |i| return false if i.nil? || i == false }
+      return true
+    end
+    count = 0
+    my_each { |i| count += 1 if yield(i) }
+    count == length
+  end
 
 end
